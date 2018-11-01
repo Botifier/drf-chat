c3ad9d8324bd7086725cdca2157904ac7f19dccf
Botifier <mouath.sai.ensi@gmail.com>
2018-11-01 03:39:48 +0100

    add channels send on new message

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import json
 from .models import Conversation, User, Message
 from .serializers import (
     MessageSerializer, 
@@ -12,7 +13,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from rest_framework_jwt.settings import api_settings
-
+from channels import Group
+from django.contrib.auth import login
 
 class ConversationList(APIView):
     
@@ -49,15 +51,27 @@ class MessageList(APIView):
         return Response(serializer.data)
 
     ''' Create new message '''
-    def post(self, request):        
+    def post(self, request): 
+        conversation = Conversation.objects.get(uid=request.data['conversation'])       
         data = {
             'sender': request.user.id,# TODO: change to name then resolve name to id
-            'conversation': Conversation.objects.get(uid=request.data['conversation']).id,
+            'conversation': conversation.id,
             'text': request.data['text']
         }
+        for user in conversation.participants.all():
+            if user.id != request.user.id:
+                receiver_id = user.id
+                break
         serializer = MessageSerializer(data=data)
         if serializer.is_valid():
-            serializer.save()
+            message = serializer.save()
+            Group(str(receiver_id)).send({
+                'text': json.dumps({
+                    'event': 'NEWMESSAGE',
+                    'conversation': str(conversation.uid),
+                    'message': str(message.uid),
+                })
+            })
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -81,13 +95,15 @@ class Registration(APIView):
     permission_classes = ()
     #TODO: add when user is already registered case
     
-    def create_user(self, username):                
+    def create_user(self, username, request):                
         password = get_random_string()
         email = get_random_string() + '@co.co'  
         try:               
             user = User.objects.create_user(username, email, password)
         except: #user already exists
             user = User.objects.get(username=username)
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(request, user)
         return user
     
     def get_token(self, user):
@@ -96,7 +112,7 @@ class Registration(APIView):
         return token
 
     def post(self, request):
-        token = self.get_token(self.create_user(request.data['username']))
+        token = self.get_token(self.create_user(request.data['username'], request))
         return Response(token, status=status.HTTP_201_CREATED)
 
 
